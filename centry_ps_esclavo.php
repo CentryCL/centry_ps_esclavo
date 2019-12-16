442bde7d9e9daae2f3bb347fd857670f720399f8
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-12-16 13:53:38 -0300

    Base modulo Centry Prestashop esclavo.

---
+++ centry_ps_esclavo.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Centry_PS_esclavo extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'Centry_PS_esclavo';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Vanessa Guzman, Yerko Cuzmar';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.6',
+            'max' => _PS_VERSION_
+        ];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Centry Esclavo');
+        $this->description = $this->l('Modulo que funciona como esclavo para Centry.');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+
+    public function install()
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        if (!parent::install() ||
+            !$this->registerHook('leftColumn') ||
+            !$this->registerHook('header')
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function uninstall()
+    {
+        if (!parent::uninstall() ||
+            !Configuration::deleteByName('MYMODULE_NAME')
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+}
