93fd539368c28ff625b9086193fe48e20bdadc69
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-12-17 16:18:24 -0300

    Credenciales obtenidas dinamicamente desde la tabla de configuracion de Prestashop.

--- classes/AuthorizationCentry.php
+++ classes/AuthorizationCentry.php
@@ -1,25 +1,23 @@
 <?php
+/**
+ * @author Yerko Cuzmar
+ */
 
 require "../sdk/CentrySDK.php";
-require "ConfiguracionCentry.php";
+require "ConfigurationCentry.php";
 
 class CentrySdk {
     public $instance = NULL;
-    private $clientId = getAttributeValue("CENTRY_SYNC_APP_ID");
-    private $clientSecret = getAttributeValue("CENTRY_SYNC_SECRET_ID");
-    private $redirectUri = getAttributeValue("CENTRY_SYNC_REDIRECT_URI");
-    private $centryScope = getAttributeValue("CENTRY_SYNC_SCOPES");
-
     function __construct()
     {
-        $this->instance = new \Centry\Sdk($this->clientId, $this->clientSecret, $this->redirectUri);
-        $this->instance = $this->instance->client_credentials($this->centryScope);
+        $this->instance = new \Centry\Sdk(getAttributeValue("CENTRY_SYNC_APP_ID"), getAttributeValue("CENTRY_SYNC_SECRET_ID"), getAttributeValue("CENTRY_SYNC_REDIRECT_URI"));
+        $this->instance = $this->instance->client_credentials(getAttributeValue("CENTRY_SYNC_SCOPES"));
     }
 
     function sdk(){
         if ($this->instance === NULL){
-            $this->instance = new \Centry\Sdk($this->clientId, $this->clientSecret, $this->redirectUri);
-            $this->instance = $this->instance->client_credentials($this->centryScope);
+            $this->instance = new \Centry\Sdk(getAttributeValue("CENTRY_SYNC_APP_ID"), getAttributeValue("CENTRY_SYNC_SECRET_ID"), getAttributeValue("CENTRY_SYNC_REDIRECT_URI"));
+            $this->instance = $this->instance->client_credentials(getAttributeValue("CENTRY_SYNC_SCOPES"));
         }
         return $this->instance;
     }
