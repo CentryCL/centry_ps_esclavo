0b1ab18f69a997f4499088d93c354ce96b3a1e07
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-12-17 13:09:31 -0300

    Credenciales obtenidas desde la tabla de configuracion de Prestashop.

--- classes/AuthorizationCentry.php
+++ classes/AuthorizationCentry.php
@@ -1,13 +1,14 @@
 <?php
 
 require "../sdk/CentrySDK.php";
+require "ConfiguracionCentry.php";
 
 class CentrySdk {
     public $instance = NULL;
-    public $clientId = "8b540ead75574333113a087d91317fcfac79bcbcc920093e029c43e306f41eb9";
-    public $clientSecret = "338f79fba22338cf6f60688614d40df9e6368301cf002f5d50e23e960b378601";
-    public $redirectUri = "urn:ietf:wg:oauth:2.0:oob";
-    public $centryScope = "public read_orders write_orders read_products write_products read_integration_config write_integration_config read_user write_user read_webhook write_webhook read_warehouses write_warehouses";
+    private $clientId = getAttributeValue("CENTRY_SYNC_APP_ID");
+    private $clientSecret = getAttributeValue("CENTRY_SYNC_SECRET_ID");
+    private $redirectUri = getAttributeValue("CENTRY_SYNC_REDIRECT_URI");
+    private $centryScope = getAttributeValue("CENTRY_SYNC_SCOPES");
 
     function __construct()
     {
