80e6603a963f1330712fb9c49b5df830e0439060
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-12-18 11:47:49 -0300

    redirect_uri, y scopes fijos.

--- classes/AuthorizationCentry.php
+++ classes/AuthorizationCentry.php
@@ -4,39 +4,44 @@
  */
 
 require_once "../sdk/CentrySDK.php";
-require_once  '../classes/ConfigurationCentry.php';
+require_once  'ConfigurationCentry.php';
 
 class CentrySdk{
     public $instance = NULL;
+    private $redirect_uri = "urn:ietf:wg:oauth:2.0:oob";
+    private $scopes = "public read_orders write_orders read_products write_products read_integration_config write_integration_config read_user write_user read_webhook write_webhook read_warehouses write_warehouses";
     function __construct()
     {
-        $this->instance = new \Centry\Sdk(getAttributeValue("CENTRY_SYNC_APP_ID"), getAttributeValue("CENTRY_SYNC_SECRET_ID"), getAttributeValue("CENTRY_SYNC_REDIRECT_URI"));
-        $this->instance = $this->instance->client_credentials(getAttributeValue("CENTRY_SYNC_SCOPES"));
+        $this->instance = new \Centry\Sdk(ConfigurationCentry::getSyncAuthAppId(), ConfigurationCentry::getSyncAuthSecretId(), $this->redirect_uri);
+        $this->instance = $this->instance->client_credentials($this->scopes);
+
     }
 
     public function sdk(){
         if ($this->instance === NULL){
-            $this->instance = new \Centry\Sdk(getAttributeValue("CENTRY_SYNC_APP_ID"), getAttributeValue("CENTRY_SYNC_SECRET_ID"), getAttributeValue("CENTRY_SYNC_REDIRECT_URI"));
-            $this->instance = $this->instance->client_credentials(getAttributeValue("CENTRY_SYNC_SCOPES"));
+            $this->instance = new \Centry\Sdk(ConfigurationCentry::getSyncAuthAppId(), ConfigurationCentry::getSyncAuthSecretId(), $this->redirect_uri);
+            $this->instance = $this->instance->client_credentials($this->scopes);
         }
         return $this->instance;
     }
 }
 
+
+print_r(ConfigurationCentry::getSyncAuthAppId());
 //$cfg = new ConfigurationCentry();
 //print_r($cfg->getAttributeValue("CENTRY_SYNC_APP_ID"));
 //
 //$sdk = new CentrySdk();
-//
+
 //$endpoint = "conexion/v1/products.json";
 //$method = "GET";
 //$params = array(
 //    "limit" => 20,
 //    "offset" => 0
 //);
-//
-//$products = $sdk->request($endpoint, $method, $params);
-//
+////
+//$products = $sdk->sdk()->request($endpoint, $method, $params);
+////
 //?><!--<!DOCTYPE html>-->
 <!--<html lang="es">-->
 <!--<head>-->
