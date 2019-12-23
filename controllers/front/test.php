0696d19628258b6319af6d367c30790692c718e0
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-12-23 11:50:11 -0300

    Test webhooks

--- controllers/front/test.php
+++ controllers/front/test.php
@@ -8,45 +8,25 @@ require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/models/Size.php';
 require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/models/Color.php';
 require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/models/Brand.php';
 require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/models/Feature.php';
-//require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/AuthorizationCentry.php';
+require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/AuthorizationCentry.php';
 
 class Centry_PS_esclavoTestModuleFrontController extends FrontController {
 
     public function initContent() {
         //parent::initContent();
-        // if(WebhookCentry::createTable()){
-        //   echo "Tabla creada Exitosamente";
-        // }
-        // else{
-        //   echo "No se pudo crear la Tabla";
-        // }
-        //
-        // if($id=WebhookCentry::getIdCentry(12)){
-        //   echo $id;
-        // }
-        // else{
-        //   echo "No existe el dato";
-        // }
-
-        // $test = new WebhookCentry();
-        // $test->id = 2;
-        // $test->id_centry = "sdfsdsasdasdg";
-        // if($test->save()){
-        //   echo "Guardado exitosamente";
-        // }
-        // else{
-        //   echo "No se pudo guardar";
-        // }
-        // 
-        // $test = new WebhookCentry(null,"sd");
-        // if($test->delete()){
-        //   echo "Borrado exitosamente";
-        // }
-        // else{
-        //   echo "No se pudo borrar";
-        // }
-
-
+        $wh = new WebhookCentry(null, null, "https://prestahop.webhook.com/asdasd", true, false, true, false);
+        error_log(print_r($wh, true));
+        $wh->id = 3;
+        $wh->getCentryWebhook();
+        error_log(print_r($wh, true));
+        $wh->callback_url = "https://prestahop.webhook.com/testUrlUpdated";
+        $wh->on_product_save = true;
+        $wh->on_product_delete = true;
+        $wh->on_order_save = true;
+        $wh->on_order_delete = true;
+        $wh->updateCentryWebhook();
+        error_log(print_r($wh, true));
+        $wh->deleteCentryWebhook();
         die();
     }
 }
