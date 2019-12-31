5b4044e6d3e1acd30911e42c52b6950ffadcf728
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-12-31 14:01:06 -0300

    Controlador de ordenes para crear y actualizar pedidos en Centry.

--- centry_ps_esclavo.php
+++ centry_ps_esclavo.php
@@ -63,87 +63,15 @@ class Centry_PS_esclavo extends Module
     }
 
     public function hookactionValidateOrder($params){
+        //TODO: encolar notificacion, todo el seteo de info de la orden se va al controlador
         error_log(print_r("hookactionValidateOrder", true));
         error_log(print_r($params, true));
-
-        $payload = array(
-            "_status" => $this->status($params["orderStatus"]->id),
-            "status_origin" => $params["orderStatus"]->name,
-            "address_billing" => $this->address($params["cart"]->id_address_invoice),
-            "address_shipping" => $this->address($params["cart"]->id_address_delivery),
-            "buyer_dni" => $params["customer"]->rut,
-            "buyer_email" => $params["customer"]->email,
-            "buyer_first_name" => $params["customer"]->firstname,
-            "buyer_last_name" => $params["customer"]->lastname,
-            "buyer_birth_date" => $params["customer"]->birthday,
-            "_buyer_gender" => $params["customer"]->id_gender == 1 ? "male" : "female",
-            "_payment_mode" => $this->paymentMode($params["order"]->payment),
-            "items" => $this->items($params["order"]->id),
-            "origin" => "Prestashop",
-            "original_data" => $params,
-            "id_origin" => $params["order"]->id_cart,
-            "number_origin" => $params["order"]->reference,
-            "total_amount" => $params["order"]->total_products_wt,
-            "shipping_amount" => $params["order"]->total_shipping,
-            "discount_amount" => $params["order"]->total_discounts,
-            "paid_amount" => $params["order"]->total_paid,
-        );
     }
 
-    private function address($id){
-        $address = new \Address($id);
-        $state = new \State($address->id_state);
-        $country = new \Country($address->id_country);
-        $array = array(
-            "first_name" => $address->firstname,
-            "last_name" => $address->lastname,
-            "phone1" => $address->phone,
-            "phone2" => $address->phone_mobile,
-            "line1" => $address->address1,
-            "line2" => $address->address2,
-            "zip_code" => $address->postcode,
-            "city" => $address->city,
-            "state" => $state->name,
-            "country" => $country->name[(int) \Configuration::get('PS_LANG_DEFAULT')]
-        );
-        return $array;
-    }
 
-    private function items($order_id){
-        $items = array();
-        $order = new Order($order_id);
-        $currency = new Currency($order->id_currency);
-        $products = $order.$this->getCartProducts();
-        foreach ($products as $product){
-            $item = array(
-                "id_origin" => $product->product_id,
-                "sku" => $product->reference,
-                "name" => $product->product_name,
-                "unit_price" => $product->unit_price_tax_incl,
-                "paid_price" => $product->total_price_tax_incl,
-                "tax_amount" => $product->total_price_tax_incl - $product->total_price_tax_excl,
-                "shipping_amount" => $product->total_shipping_price_tax_incl,
-                "currency" => $currency->iso_code,
-                "quantity" => $product->product_quantity,
-            );
-        }
-        return products;
-    }
-
-    private function paymentMode($payment){
-        switch ($payment){
-            case "Bank Transfer":
-                return "transfer";
-            default:
-                return "undefined";
-        }
-    }
-
-    private function status($id_order_state){
-
-    }
 
     public function hookactionOrderHistoryAddAfter($params){
+        //TODO: encolar notificacion, todo el seteo de info de la orden se va al controlador
         error_log(print_r("hookactionOrderHistoryAddAfter", true));
         error_log(print_r($params, true));
     }
---
+++ controllers/front/order_controller.php
@@ -0,0 +1,119 @@
+<?php
+require_once _PS_MODULE_DIR_ . 'centry_ps_esclavo/classes/models/Order.php';
+
+class OrderController extends FrontController
+{
+    public function createOrderCentry($order_id){
+        $order = OrderController::ordertoCentry($order_id);
+        if($order){
+            $resp = AuthorizationCentry::sdk()->createOrder(null, $order);
+            if($resp){
+                $order_homol = new OrderCentry($order_id, $resp->_id);
+                return $order_homol->save();
+            }
+        }
+        return false;
+    }
+
+    public function updateOrderCentry($order_id){
+        $centry_order_id = OrderCentry::getIdCentry($order_id)[0]["id_centry"];
+        error_log(print_r($centry_order_id,true));
+        if($centry_order_id){
+            $order = OrderController::ordertoCentry($order_id);
+            if($order){
+                $resp = AuthorizationCentry::sdk()->updateOrder($centry_order_id, null, $order);
+                return $resp;
+            }
+        }
+        return false;
+    }
+
+    public function orderToCentry($order_id){
+        $order = new Order($order_id);
+        if($order){
+            $order_status = $order->getCurrentStateFull(Configuration::get('PS_LANG_DEFAULT'));
+            $customer = new Customer($order->id_customer);
+            $cart = new Cart($order->id_cart);
+
+            $payload = array(
+                "_status" => OrderStatusCentry::getCentryStatus($order_status["id_order_state"]),
+                "status_origin" => $order_status["name"],
+                "address_billing" => OrderController::address($cart->id_address_invoice),
+                "address_shipping" => OrderController::address($cart->id_address_delivery),
+//                "buyer_dni" => $customer->rut,
+                "buyer_email" => $customer->email,
+                "buyer_first_name" => $customer->firstname,
+                "buyer_last_name" => $customer->lastname,
+                "buyer_birth_date" => $customer->birthday, //TODO: revisar
+                "_buyer_gender" => $customer->id_gender == 1 ? "male" : "female",
+                "_payment_mode" => OrderController::paymentMode($order->payment),
+                "items" => OrderController::items($order->id),
+                "origin" => "Prestashop",
+                "original_data" => array("order" => $order, "cart" => $cart, "order_status" => $order_status, "customer" => $customer), //TODO: revisar
+                "id_origin" => $order->id_cart,
+                "number_origin" => $order->reference,
+                "total_amount" => $order->total_products_wt,
+                "shipping_amount" => $order->total_shipping,
+                "discount_amount" => $order->total_discounts,
+                "paid_amount" => $order->total_paid,
+            );
+            return $payload;
+        }
+        return false;
+    }
+
+    private function address($id){
+        $address = new \Address($id);
+        $state = new \State($address->id_state);
+        $country = new \Country($address->id_country);
+        if($address){
+            $array = array(
+                "first_name" => $address->firstname,
+                "last_name" => $address->lastname,
+                "phone1" => $address->phone,
+                "phone2" => $address->phone_mobile,
+                "line1" => $address->address1,
+                "line2" => $address->address2,
+                "zip_code" => $address->postcode,
+                "city" => $address->city,
+                "state" => $state->name,
+                "country" => $country->name[(int) \Configuration::get('PS_LANG_DEFAULT')]
+            );
+        }
+        return $array;
+    }
+
+    private function items($order_id){
+        $items = array();
+        $order = new Order($order_id);
+        $currency = new Currency($order->id_currency);
+        $products = $order->getCartProducts();
+        foreach ($products as $product){
+//            error_log(print_r($product), true);
+            $item = array(
+                "id_origin" => $product["product_id"],
+                "sku" => $product["reference"],
+                "name" => $product["product_name"],
+                "unit_price" => $product["unit_price_tax_incl"],
+                "paid_price" => $product["total_price_tax_incl"],
+                "tax_amount" => $product["total_price_tax_incl"] - $product["total_price_tax_excl"],
+                "shipping_amount" => $product["total_shipping_price_tax_incl"],
+                "currency" => $currency->iso_code,
+                "quantity" => $product["product_quantity"],
+            );
+            array_push($items, $item);
+        }
+        return $items;
+    }
+
+    private function paymentMode($payment){
+        switch ($payment){
+            case "Bank Transfer":
+                return "transfer";
+            default:
+                return "undefined";
+        }
+    }
+
+
+}
