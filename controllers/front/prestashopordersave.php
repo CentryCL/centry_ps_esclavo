04c6aab677d38a69d1b0b51c28744f124f615f66
Elías Lama L. <eliaslama@gmail.com>
2020-01-06 16:01:53 -0300

    Creación de los controladores faltantes y necesarios para la ejecución de las tareas asíncronas de un módulo esclavo.

---
+++ controllers/front/prestashopordersave.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../vendor/autoload.php');
+require_once dirname(__FILE__) . '/abstracttaskprocessor.php';
+
+use CentryPs\enums\system\PendingTaskOrigin;
+use CentryPs\enums\system\PendingTaskTopic;
+use CentryPs\models\system\PendingTask;
+
+/**
+ * Controlador encargado de ejecutar la tarea de leer un producto de Centry para
+ * crearlo o actualizarlo en Prestashop.
+ */
+class Centry_Ps_EsclavoPrestashopOrderSaveModuleFrontController extends AbstractTaskProcessor {
+
+  protected $origin = PendingTaskOrigin::PrestaShop;
+  protected $topic = PendingTaskTopic::OrderSave;
+  
+  protected function processTask(PendingTask $task) {
+    error_log(print_r($task, true));
+    throw new Exception('Unimplemented method');
+  }
+
+}
+
+
