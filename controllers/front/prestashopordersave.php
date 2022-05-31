90c4a3dc6b75b2c44cb8915c62a63be1d60b8ba7
Elias Lama L <eliaslama@gmail.com>
2022-05-31 16:46:59 -0400

    Corrección de capitalización.

--- controllers/front/prestashopordersave.php
+++ controllers/front/prestashopordersave.php
@@ -20,7 +20,7 @@ class Centry_Ps_EsclavoPrestashopOrderSaveModuleFrontController extends Abstract
   protected function processTask(PendingTask $task) {
     $order_id_ps = $task->resource_id;
     $order_id_centry = CentryPs\models\homologation\Order::getIdCentry($order_id_ps);
-    $payload = CentryPs\translators\Orders::ordertoCentry($order_id_ps);
+    $payload = CentryPs\translators\Orders::orderToCentry($order_id_ps);
     if ($order_id_centry) {
       AuthorizationCentry::sdk()->updateOrder($order_id_centry, null, $payload);
     } else {
